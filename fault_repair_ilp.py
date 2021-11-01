bc87b5d44f35f4555a1cdf6bb28685e2593761e3
David Zhao <dzha3983@uni.sydney.edu.au>
2021-11-01 17:57:08 +1100

    Refactor fault_repair_ilp

--- fault_repair_ilp.py
+++ fault_repair_ilp.py
@@ -20,101 +20,7 @@ import pulp
 # - construct a minimum set cover problem
 # - run some approximation algorithm for the minimum set cover
 
-def construct_minimum_set_cover(trees):
-    # trees is a list of lists of tuples
-    # e.g. [[e(1, 2), e(2, 3)], [e(1, 4), e(4, 3)]]
-
-    # the minimum set cover instance should have the set of tuples as a carrier
-    # set, with each subset being the set of proof trees that the tuple appears
-    # in
-
-    set_cover_problem = collections.defaultdict(set)
-
-    for (idx, tree) in enumerate(trees):
-        for tup in tree:
-            set_cover_problem[tup].add(idx)
-
-    return set_cover_problem
-
-def solve_minimum_set_cover(trees, set_cover_problem):
-    # a very simple greedy algorithm that gives a ln(n) approximation (or
-    # something like that)
-
-    # ILP SOLUTION STARTS HERE
-    if len(set_cover_problem) == 0:
-        return []
-
-    set_cover_ilp = pulp.LpProblem("Set Cover", pulp.LpMinimize)
-    tuples = pulp.LpVariable.dicts("tuples", set_cover_problem.keys(), cat='Integer')
-
-    for tup in set_cover_problem.keys():
-        tuples[tup].bounds(0, 1)
-
-    # objective function
-    set_cover_ilp += sum([tuples[tup] for tup in set_cover_problem.keys()])
-
-    # constraints that every tree must be covered
-    for (idx, _) in enumerate(trees):
-        set_cover_ilp += pulp.lpSum([tuples[tup] for tup in set_cover_problem.keys() if idx in set_cover_problem[tup]]) >= 1
-
-    # solve
-    set_cover_ilp.solve(pulp.apis.PULP_CBC_CMD(logPath=""))
-
-    # get result
-    selected_tuples = []
-
-    for tup in set_cover_problem.keys():
-        if tuples[tup].varValue == 1:
-            selected_tuples.append(tup)
-
-    return selected_tuples
-
-    # APPROXIMATION SOLUTION STARTS HERE
-    # if len(set_cover_problem) == 0:
-    #     return []
-
-    # covered = [False for t in trees]
-    # tuples = []
-
-    # while not all(covered):
-    #     # take max number of covered elements
-    #     m = max(set_cover_problem.items(), key=(lambda x: len(x[1])))
-
-    #     # remove m from set_cover_problem
-    #     set_cover_problem.pop(m[0])
-
-    #     # invalidate trees covered by m
-    #     for t in m[1]:
-    #         covered[t] = True
-
-    #         for tup in trees[t]:
-    #             # print("discarding", t, "from", tup)
-    #             set_cover_problem[tup].discard(t)
-
-    #     # print("covered:", len(covered))
-
-    #     # print("set cover problem:", set_cover_problem)
-
-    #     uncovered = []
-    #     for (idx, c) in enumerate(covered):
-    #         if c == False:
-    #             uncovered.append(idx)
-
-    #     # print(uncovered)
-
-    #     tuples.append(m[0])
-
-    # return tuples
-
-def repair_faults(souffle_instance, faults):
-    # The idea here is to create an ILP based on the proof trees for the faults.
-    # For each one-step proof t_1, ..., t_k => t, we create the ILP constraint
-    # t_1 + ... + t_k - t <= k - 1
-    # 
-    # Each t_i is a 0-1 integer
-    # The fault tuple t_bad = 0
-    # The optimization constraint is to maximize sum of {EDB tuples t}
-
+def construct_repair_ilp(trees):
     # initialize ILP problem instance
     repair_ilp = pulp.LpProblem("Repair", pulp.LpMaximize)
 
@@ -169,7 +75,7 @@ def repair_faults(souffle_instance, faults):
                 constraint_body = [head_tup]
                 constraint_body += current_body_tuples
                 constraint = pulp.lpSum([ilp_vars[x] * (-1 if x == head_tup else 1) for x in constraint_body]) <= len(current_body_tuples) - 1
-                print("constraint:", constraint, "premises:", head_tup, "body_tups:", current_body_tuples)
+                # print("constraint:", constraint, "premises:", head_tup, "body_tups:", current_body_tuples)
                 repair_ilp += constraint
             else:
                 # something is wrong
@@ -181,26 +87,59 @@ def repair_faults(souffle_instance, faults):
 
             cNum += 1
 
-    # get json and walk it
-    for f in faults:
-        tree = faultbase.getAllProvJson(souffle_instance, f)
+    for tree in trees:
         walk_prov_tree(tree)
         fault_constraint = (ilp_vars[tree['premises']] == 0)
-        print("fault constraint:", fault_constraint)
+
+        # print("fault constraint:", fault_constraint)
         repair_ilp += fault_constraint
 
     # create maximization target
     target = pulp.lpSum([ilp_vars[e] for e in edb_tuples])
-    print("target:", target)
+    # print("target:", target)
     repair_ilp += target
 
-    repair_ilp.solve(pulp.apis.PULP_CBC_CMD(logPath=""))
+    return (edb_tuples, ilp_vars, repair_ilp)
+
+def solve_repair_ilp(edb_tuples, ilp_vars, ilp):
+    ilp.solve(pulp.apis.GLPK_CMD())
+    # ilp.solve()
 
     repair = set()
     for tup in edb_tuples:
-        print(tup, ilp_vars[tup].varValue)
+        # print(tup, ilp_vars[tup].varValue)
         if ilp_vars[tup].varValue == 0:
             repair.add(tup)
+
+    return repair
+
+def repair_faults(souffle_instance, faults):
+    # The idea here is to create an ILP based on the proof trees for the faults.
+    # For each one-step proof t_1, ..., t_k => t, we create the ILP constraint
+    # t_1 + ... + t_k - t <= k - 1
+    # 
+    # Each t_i is a 0-1 integer
+    # The fault tuple t_bad = 0
+    # The optimization constraint is to maximize sum of {EDB tuples t}
+
+
+    # get json and walk it
+
+    trees = []
+    for f in faults:
+        tree = faultbase.getAllProvJson(souffle_instance, f)
+        if 'axiom' in tree and tree['axiom'] == 'Tuple not found':
+            continue
+
+        trees.append(tree)
+
+    if trees == []:
+        return ([], [])
+
+    (edb_tuples, ilp_vars, repair_ilp) = construct_repair_ilp(trees)
+
+    # repair_ilp.solve(pulp.apis.PULP_CBC_CMD(logPath=""))
+    repair = solve_repair_ilp(edb_tuples, ilp_vars, repair_ilp)
 
     return repair
 
